926ea6cec6391ad29961abee63e2a1177c5e25de
Roberto Ramírez Ontiveros <idv18c.rramirez@uartesdigitales.edu.mx>
2022-01-29 20:39:03 -0600

    Ya cambia de nivel

---
+++ Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour
+{
+    RaycastHit2D lightLine;
+
+    [SerializeField] GameObject player;
+
+    [SerializeField] GameObject finalPoint;
+
+    public string levelName;
+
+
+    private float distance;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(lightBlock.transform.localScale);
+        distance = (this.transform.position - finalPoint.transform.position).magnitude;
+
+        lightLine = Physics2D.Raycast(this.transform.position, Vector2.down,
+                                     distance);
+
+
+        if (lightLine.collider.name == player.name)
+        {
+            Debug.DrawRay(this.transform.position, Vector2.down * distance, Color.green);
+            Debug.Log(lightLine.collider.name);
+            SceneManager.LoadScene(levelName);
+        }
+
+        if (lightLine.collider == null)
+        {
+            Debug.DrawRay(this.transform.position, Vector2.down * distance, Color.red);
+        }
+    }
+}
